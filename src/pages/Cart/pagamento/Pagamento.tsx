15f119c255cb9cc22294c86b6f149b49dd652fce
K4uePinheiro <kauegabriel1982@gmail.com>
2025-11-14 17:14:59 -0300

    Update error

--- src/pages/Cart/pagamento/Pagamento.tsx
+++ src/pages/Cart/pagamento/Pagamento.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import 'src/pages/Cart/pagamento/pagamento.css'
+import './Pagamento.css'
 import CheckoutProgress from "../Checkout/CheckoutProgress";
 
 
